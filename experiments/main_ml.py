a3724efbae1cc4d872d0b7fae9efc9104d390049
mcarranzag <mcarranzag@us.es>
2021-10-27 13:10:35 -0700

    First commit

---
+++ experiments/main_ml.py
@@ -0,0 +1,180 @@
+import os
+import time
+import json
+from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import ParameterGrid
+from sklearn.multioutput import MultiOutputRegressor
+from xgboost import XGBRegressor
+
+from utils import data, metrics
+
+FREQ = "1min"
+N_DAYS_TEST = 10
+PAST_HISTORY = 240
+FORECASTING_HORIZON = 60
+SCALER = "MinMax"
+DATA_PATH = "./data"
+OUTPUT_PATH = "./results"
+N_JOBS = -1
+
+
+def RandomForest():
+    return RandomForestRegressor()
+
+
+def XGBoost():
+    return MultiOutputRegressor(XGBRegressor())
+
+
+MODELS = {
+    RandomForest: {
+        "n_estimators": [100, 500],
+        "max_depth": [10, 100, None],
+        "min_samples_split": [2, 10],
+        "min_samples_leaf": [1, 4],
+        "bootstrap": [True, False],
+        "n_jobs": [N_JOBS],
+    },
+    XGBoost: {
+        "estimator__learning_rate": [0.05, 0.10, 0.25],
+        "estimator__max_depth": [3, 10],
+        "estimator__min_child_weight": [1, 5],
+        "estimator__gamma": [0.1, 0.4],
+        "estimator__colsample_bytree": [0.3, 0.7],
+        "estimator__n_jobs": [N_JOBS],
+    }
+}
+
+
+def preprocess_data(df_):
+    df = df_.copy()
+    end_date = df.index.max()
+    df = data.transform_to_evenly_spaced(df, freq=FREQ)
+    df, scaler = data.scale_features(df, scaler=SCALER, n_days_test=N_DAYS_TEST)
+    df = data.build_features(df, past_history=PAST_HISTORY, forecasting_horizon=FORECASTING_HORIZON)
+    df_train, df_test = data.split_train_test(df, end_date=end_date, n_days_test=N_DAYS_TEST)
+    X_train, y_train = data.split_input_output(df_train)
+    X_test, y_test = data.split_input_output(df_test)
+    return X_train, y_train, X_test, y_test, scaler, df_test.index
+
+
+def evaluate(actual, predicted):
+    ans = dict()
+    for m in metrics.METRICS:
+        ans[m] = metrics.METRICS[m](actual, predicted)
+    return ans
+
+
+def save_results(
+    y_test_unscaled,
+    y_pred_unscaled,
+    errors,
+    test_index,
+    dataset_name,
+    model_name,
+    model_index,
+    model_params,
+    train_time,
+    test_time,
+    train_parameters,
+):
+    out_path = f"{OUTPUT_PATH}/FH{FORECASTING_HORIZON}-PH{PAST_HISTORY}/{dataset_name}"
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+
+    # Save predictions
+    y_pred_df = pd.DataFrame(y_pred_unscaled)
+    y_pred_df.index = test_index
+    y_pred_df.to_csv(f"{out_path}/o_{model_name}_{model_index}.csv", float_format="%.8f")
+
+    # Save Y values
+    y_test_df = pd.DataFrame(y_test_unscaled)
+    y_test_df.index = test_index
+    y_test_df.to_csv(f"{out_path}/y.csv", float_format="%.1f")
+
+    # Save metrics
+    errors_df = pd.DataFrame(errors)
+    errors_df.columns = [c.upper() for c in errors_df.columns]
+    errors_df = errors_df[[c for c in sorted(errors_df.columns)]]
+    errors_df.index = test_index
+    errors_df.to_csv(f"{out_path}/metrics_{model_name}_{model_index}.csv", float_format="%.8f")
+
+    # Save model parameters
+    models = {
+        "Dataset": [dataset_name],
+        "Model": [f"{model_name}_{model_index}"],
+        "Model architecture": [model_name],
+        "Model index": [model_index],
+        "Model params": [json.dumps(model_params)],
+        "Other params": [json.dumps(train_parameters)],
+        "Training time": [train_time],
+        "Inference time": [test_time],
+    }
+    models_df = pd.DataFrame(models)
+    if Path(f"{out_path}/models.csv").exists():
+        models_df_tmp = pd.read_csv(f"{out_path}/models.csv")
+        models_df = models_df_tmp.append(models_df, ignore_index=True)
+    models_df.to_csv(f"{out_path}/models.csv", index=False, float_format="%.8f")
+
+    # Save summary of results
+    results = metrics.evaluate_all(y_test_unscaled, y_pred_unscaled)
+    results = {k.upper(): [results[k]] for k in sorted(results.keys())}
+    results = {**models, **results}
+    results = pd.DataFrame(results)
+    if Path(f"{OUTPUT_PATH}/results.csv").exists():
+        results_tmp = pd.read_csv(f"{OUTPUT_PATH}/results.csv")
+        results = results_tmp.append(results, ignore_index=True)
+    results.to_csv(f"{OUTPUT_PATH}/results.csv", index=False, float_format="%.8f")
+
+
+files = sorted(os.listdir(DATA_PATH))
+
+for filename in files:
+    dataset_name = filename.replace(".csv", "")
+    print(dataset_name)
+
+    df = data.read_dataframe(f"{DATA_PATH}/{filename}")
+    X_train, y_train, X_test, y_test, scaler, test_index = preprocess_data(df)
+
+    for model_func in MODELS:
+        grid = ParameterGrid(MODELS[model_func])
+        for idx, params in tqdm(list(enumerate(grid)), desc=model_func.__name__):
+            model = model_func()
+            model.set_params(**params)
+
+            t0 = time.time()
+            model.fit(X_train, y_train)
+            train_time = time.time() - t0
+
+            t0 = time.time()
+            y_pred = model.predict(X_test)
+            inference_time = time.time() - t0
+
+            y_pred_unscaled = scaler.inverse_transform(y_pred)
+            y_test_unscaled = scaler.inverse_transform(y_test)
+
+            errors = [evaluate(y, o) for y, o in zip(y_test_unscaled, y_pred_unscaled)]
+
+            train_parameters = {
+                "Forecasting horizon": FORECASTING_HORIZON,
+                "Past history": PAST_HISTORY,
+                "Scaler": SCALER,
+                "Data frequency": FREQ,
+                "Number days test": N_DAYS_TEST,
+            }
+            save_results(
+                y_test_unscaled,
+                y_pred_unscaled,
+                errors,
+                test_index,
+                dataset_name,
+                model_func.__name__,
+                idx,
+                params,
+                train_time,
+                inference_time,
+                train_parameters,
+            )
---
+++ experiments/utils/data.py
@@ -0,0 +1,93 @@
+import datetime as dt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.base import BaseEstimator
+
+RAW_COLUMNS = ["start_time", "value"]
+INDEX_COL = "Datetime"
+VALUE_COL = "Value"
+COLUMNS = [INDEX_COL, VALUE_COL]
+
+
+def read_dataframe(filename):
+    df = pd.read_csv(filename)
+    df = df[RAW_COLUMNS]
+    df.columns = COLUMNS
+    df[VALUE_COL] = df[VALUE_COL].astype("float")
+    df[INDEX_COL] = pd.to_datetime(df[INDEX_COL])
+    df.index = df.pop(INDEX_COL)
+    return df
+
+
+def transform_to_evenly_spaced(df, freq="1min", method="backfill"):
+    df = df.asfreq(freq, method=method)
+    return df
+
+
+def _build_past_history_features(df_, past_history, value_column):
+    df = df_.copy()
+    for i in range(past_history, 0, -1):
+        df[f"X_{i}"] = df[value_column].shift(i)
+    return df
+
+
+def _build_forecasting_horizon_features(df_, forecasting_horizon, value_column):
+    df = df_.copy()
+    for i in range(forecasting_horizon):
+        df[f"y_{i}"] = df[value_column].shift(-i)
+    return df
+
+
+def build_features(df_, past_history, forecasting_horizon, value_column=VALUE_COL):
+    df = df_.copy()
+    df = _build_past_history_features(df, past_history, value_column)
+    df = _build_forecasting_horizon_features(df, forecasting_horizon, value_column)
+    df = df.drop(value_column, axis=1)
+    df = df.dropna()
+    return df
+
+
+def split_train_test(df, end_date=None, n_days_test=10):
+    if end_date == None:
+        end_date = df.index.max()
+    start_test = end_date - dt.timedelta(days=n_days_test)
+    df_train = df[df.index <= start_test].copy()
+    df_test = df[df.index > start_test].copy()
+    return df_train, df_test
+
+
+def scale_features(
+    df_, scaler="MinMax", end_date=None, n_days_test=10, value_column=VALUE_COL
+):
+    df = df_.copy()
+
+    sc = None
+    if isinstance(scaler, BaseEstimator):
+        sc = scaler
+    if isinstance(scaler, str):
+        if scaler.upper() == "MINMAX":
+            sc = MinMaxScaler()
+        elif scaler.upper() == "STANDARD":
+            sc = StandardScaler()
+    if sc == None:
+        raise ValueError(
+            "scaler must be instance of sklearn.base.BaseEstimator or one of the following string: 'MinMax', 'Standard'"
+        )
+
+    if end_date == None:
+        end_date = df.index.max()
+
+    start_test = end_date - dt.timedelta(days=n_days_test)
+    sc = sc.fit(df[df.index <= start_test][[value_column]])
+
+    df[[value_column]] = sc.transform(df[[value_column]])
+    return df, sc
+
+
+def split_input_output(df_):
+    df = df_.copy()
+    y_cols = [c for c in df.columns if c.startswith("y_")]
+    X_cols = [c for c in df.columns if c.startswith("X_")]
+    y = df[y_cols].values
+    X = df[X_cols].values
+    return X, y
