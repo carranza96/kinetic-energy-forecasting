4b4779f5db899c859c6aad4243ac4a9697533748
carranza96 <manuelcarranza96@gmail.com>
2021-11-09 23:27:50 +0100

    Update main

--- run_one.sh
+++ run_one.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Parameters
-FORECASTING_HORIZON=60
-PAST_HISTORY=240
+FORECASTING_HORIZON=96
+PAST_HISTORY=384
+FREQUENCY=15min
 
-echo '#######' 'FH'$FORECASTING_HORIZON-'PH'$PAST_HISTORY '#######'
-python ./experiments/main.py --forecasting-horizon $FORECASTING_HORIZON --past-history $PAST_HISTORY
+echo '#######' 'FH'$FORECASTING_HORIZON-'PH'$PAST_HISTORY-'FREQ'$FREQUENCY '#######'
+python ./experiments/main.py --forecasting-horizon $FORECASTING_HORIZON --past-history $PAST_HISTORY --frequency $FREQUENCY
